2afc3dc5be8732301e274cde61abd7084dd14c3a
Jens Utbult <jens.utbult@gmail.com>
2016-01-15 13:20:49 +0000

    Added acc and mag output

--- RaspberryPi/lsm9ds0.js
+++ RaspberryPi/lsm9ds0.js
@@ -5,14 +5,23 @@ var EventEmitter = require('events').EventEmitter;
 function LSM9DS0() {
     var self = this
     
-    this.acc = new i2c(0x1d, {device: '/dev/i2c-1'});
+    this.xm = new i2c(0x1d, {device: '/dev/i2c-1'});
+    // z,y,x axis enabled, continuos update,  100Hz data rate
+    this.xm.writeBytes(LSM9DS0.CTRL_REG1_XM, [0x67], function(err){});
+    // 6G scale
+    this.xm.writeBytes(LSM9DS0.CTRL_REG2_XM, [0x10], function(err){});
+    
+    // initialize mag    
+    this.xm.writeBytes(LSM9DS0.CTRL_REG5_XM, [0xF0], function(err){});
+    this.xm.writeBytes(LSM9DS0.CTRL_REG6_XM, [0x60], function(err){});
+    this.xm.writeBytes(LSM9DS0.CTRL_REG7_XM, [0x00], function(err){});
     
     // initialize gyro
     this.gyro = new i2c(0x6b, {device: '/dev/i2c-1'});
     this.gyro.writeBytes(LSM9DS0.CTRL_REG1_G, [0x0F], function(err){});
     this.gyro.writeBytes(LSM9DS0.CTRL_REG4_G, [0x30], function(err){});
     this.gyroBias = 0
-    
+
     this.time = process.hrtime();
     self.calculateGyroBias(0, function() {
 	self.emitGyro();
@@ -43,6 +52,10 @@ LSM9DS0.prototype.calculateGyroBias = function(counter, callback) {
 
 LSM9DS0.prototype.emitGyro = function() {
     var self = this;
+//    this.readMag(function(data) {
+//	console.log(data)
+//    })
+    
     this.readGyro(function(data) {
 	var dt = process.hrtime(self.time)[1] / 1000000000.0;
 	self.time = process.hrtime()
@@ -62,6 +75,23 @@ LSM9DS0.prototype.readGyro = function(callback) {
     });
 };
 
+LSM9DS0.prototype.readAcc = function(callback) {
+    this.xm.readBytes(0x80 | LSM9DS0.OUT_X_L_A, 6, function(error, data) {
+	var x = data.readInt16LE(0);
+	var y = data.readInt16LE(2);
+	var z = data.readInt16LE(4);
+	callback({'x':x, 'y':y, 'z':z});
+    });
+};
+
+LSM9DS0.prototype.readMag = function(callback) {
+    this.xm.readBytes(0x80 | LSM9DS0.OUT_X_L_M, 6, function(error, data) {
+	var x = data.readInt16LE(0);
+	var y = data.readInt16LE(2);
+	var z = data.readInt16LE(4);
+	callback({'x':x, 'y':y, 'z':z});
+    });
+};
 
 
 LSM9DS0.WHO_AM_I_G = 0x0F;
@@ -110,6 +140,65 @@ LSM9DS0.G_ODR_760_BW_30 = 0xC;  //   760         30
 LSM9DS0.G_ODR_760_BW_35 = 0xD;  //   760         35
 LSM9DS0.G_ODR_760_BW_50 = 0xE;  //   760         50
 LSM9DS0.G_ODR_760_BW_100 = 0xF; //   760         100
+
+
+LSM9DS0.OUT_TEMP_L_XM = 0x05;
+LSM9DS0.OUT_TEMP_H_XM = 0x06;
+LSM9DS0.STATUS_REG_M = 0x07;
+LSM9DS0.OUT_X_L_M = 0x08;
+LSM9DS0.OUT_X_H_M = 0x09;
+LSM9DS0.OUT_Y_L_M = 0x0A;
+LSM9DS0.OUT_Y_H_M = 0x0B;
+LSM9DS0.OUT_Z_L_M = 0x0C;
+LSM9DS0.OUT_Z_H_M = 0x0D;
+LSM9DS0.WHO_AM_I_XM = 0x0F;
+LSM9DS0.INT_CTRL_REG_M = 0x12;
+LSM9DS0.INT_SRC_REG_M = 0x13;
+LSM9DS0.INT_THS_L_M = 0x14;
+LSM9DS0.INT_THS_H_M = 0x15;
+LSM9DS0.OFFSET_X_L_M = 0x16;
+LSM9DS0.OFFSET_X_H_M = 0x17;
+LSM9DS0.OFFSET_Y_L_M = 0x18;
+LSM9DS0.OFFSET_Y_H_M = 0x19;
+LSM9DS0.OFFSET_Z_L_M = 0x1A;
+LSM9DS0.OFFSET_Z_H_M = 0x1B;
+LSM9DS0.REFERENCE_X = 0x1C;
+LSM9DS0.REFERENCE_Y = 0x1D;
+LSM9DS0.REFERENCE_Z = 0x1E;
+LSM9DS0.CTRL_REG0_XM = 0x1F;
+LSM9DS0.CTRL_REG1_XM = 0x20;
+LSM9DS0.CTRL_REG2_XM = 0x21;
+LSM9DS0.CTRL_REG3_XM = 0x22;
+LSM9DS0.CTRL_REG4_XM = 0x23;
+LSM9DS0.CTRL_REG5_XM = 0x24;
+LSM9DS0.CTRL_REG6_XM = 0x25;
+LSM9DS0.CTRL_REG7_XM = 0x26;
+LSM9DS0.STATUS_REG_A = 0x27;
+LSM9DS0.OUT_X_L_A = 0x28;
+LSM9DS0.OUT_X_H_A = 0x29;
+LSM9DS0.OUT_Y_L_A = 0x2A;
+LSM9DS0.OUT_Y_H_A = 0x2B;
+LSM9DS0.OUT_Z_L_A = 0x2C;
+LSM9DS0.OUT_Z_H_A = 0x2D;
+LSM9DS0.FIFO_CTRL_REG = 0x2E;
+LSM9DS0.FIFO_SRC_REG = 0x2F;
+LSM9DS0.INT_GEN_1_REG = 0x30;
+LSM9DS0.INT_GEN_1_SRC = 0x31;
+LSM9DS0.INT_GEN_1_THS = 0x32;
+LSM9DS0.INT_GEN_1_DURATION = 0x33;
+LSM9DS0.INT_GEN_2_REG = 0x34;
+LSM9DS0.INT_GEN_2_SRC = 0x35;
+LSM9DS0.INT_GEN_2_THS = 0x36;
+LSM9DS0.INT_GEN_2_DURATION = 0x37;
+LSM9DS0.CLICK_CFG = 0x38;
+LSM9DS0.CLICK_SRC = 0x39;
+LSM9DS0.CLICK_THS = 0x3A;
+LSM9DS0.TIME_LIMIT = 0x3B;
+LSM9DS0.TIME_LATENCY = 0x3C;
+LSM9DS0.TIME_WINDOW = 0x3D;
+LSM9DS0.ACT_THS = 0x3E;
+LSM9DS0.ACT_DUR = 0x3F;
+
 
 
 module.exports.LSM9DS0 = LSM9DS0;
